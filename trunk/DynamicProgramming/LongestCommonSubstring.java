fe85949db2f61693058f93524a803d7c8d562cd7
Brian <bjk33@zips>
2012-08-01 20:15:30 -0400

    first run of longest common substring

--- trunk/DynamicProgramming/LongestCommonSubstring.java
+++ trunk/DynamicProgramming/LongestCommonSubstring.java
@@ -13,17 +13,37 @@ public class LongestCommonSubstring {
 	public static String longestCommonSubstring(String s1, String s2) {
 	
 		// create a table with s1 as the top and s2 as the left
-		int[][] ar = new int[s1.length() + 1][s2.length + 1];
+		int rows = s1.length() + 1;
+		int cols = s2.length() + 1;
+		int[][] ar = new int[rows][cols];
 		
 		// fill the table
-		for (int i = 1; i <= s1.length(); ++i) {
-			for (int j = 1; j <= s2.length(); ++j) {
+		for (int i = 1; i < rows; ++i) {
+			for (int j = 1; j < cols; ++j) {
+			
+				// get the max of the left value and the above value
+				int left = ar[i][j - 1];
+				int above = ar[i - 1][j];
+				int max = Math.max(left, above);
 				
-				// it's a match, so add on the diagonal
-				if (s1.charAt(i) == s2.charAt(j))
-					ar[i][j] = ar[i - 1][j - 1] + 1;
+				// if it's a match, consider the diagonal
+				if (s1.charAt(j - 1) == s2.charAt(i - 1))
+					max = Math.max(ar[i - 1][j - 1] + 1, max);
+					
+				// store the max
+				ar[i][j] = max;
+				
+				System.out.print("s1: " + s1.charAt(j - 1));
+				System.out.print(" s2: " + s2.charAt(i - 1));
+				System.out.print(" value: " + ar[i][j]);
+				System.out.println();
 			}
+			
+			System.out.println();
 		}
+		
+		// output the max value
+		System.out.println(ar[rows - 1][cols - 1]);
 		
 		return "";
 	}
