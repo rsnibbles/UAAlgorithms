e8057a0086836e3aa6d145281265cc0fe106629e
Jeff Chapman II <jac@JAChapmanII.net>
2012-08-05 09:18:14 -0400

    Cleanup and fix DFS

--- src/main/GraphAlgorithms/Trees/DFS/DFS.java
+++ src/main/GraphAlgorithms/Trees/DFS/DFS.java
@@ -2,43 +2,38 @@ import java.util.*;
 
 public class DFS
 {
-	// given the an array of nodes, will traverse the entire tree in a depth first search,
-	// starting from the node at graph[0]
-
-/*
-This doesn't compile so I have commented it out until it is fixed.
-
-
+	// given the an array of nodes, will traverse the entire tree in a depth
+	// first search, starting from the node at graph[0]
 	public void DFS(ArrayList<Node> graph)
 	{
-	
 		ArrayList<Node> stack = new ArrayList<Node>();
 
-		stack.add(graph[0]);
-		stack[0].visited = true; // so that we don't visit it again
-		while(!queue.isEmpty())
+		stack.add(graph.get(0));
+		stack.get(0).visited = true; // so that we don't visit it again
+		while(!stack.isEmpty())
 		{
-			Node cur_node = stack.remove(stack.size()-1); // removes the node on the top of the stack
-			cur_node.visited = true;
-			for(int index = 0; index < cur_node.children.size(); index++)
+			// remove the node on the top of the stack
+			Node cur_node = stack.remove(stack.size() - 1);
+			for(Node child : cur_node.children)
 			{
-				if(!cur_node.children[index].visited)
+				if(!child.visited)
 				{
-					stack.add(cur_node.children[index]);
-					cur_node.children[index].visited = true;
+					stack.add(child);
+					child.visited = true;
 				}
 			}
 		}
-
 	}
 
-	class Node
+	public static class Node
 	{
+		public ArrayList<Node> children;
+		public boolean visited = false;
+
 		public Node(ArrayList<Node> childs)
 		{
-			ArrayList<Node> children = new Arraylist<Node>(childs);
-			boolean visited = false;
+			this.children = new ArrayList<Node>(childs);
+			this.visited = false;
 		}
 	}
-*/
 }
