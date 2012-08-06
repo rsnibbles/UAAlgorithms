caf03653675441fd944000d500c0e067df89a34f
Jeff Chapman II <jac@JAChapmanII.net>
2012-08-06 15:07:34 -0400

    Print the number of failed tests, return that

--- runtests.sh
+++ runtests.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-find target/test -type f -name '*.class' | while read tname; do
+total_failed=0
+while read tname; do
 	tname="$(basename "$tname" | cut -d'.' -f1)"
 	./run.sh "$tname"
-done
+	total_failed=$((total_failed + $?))
+done < <(find target/test -type f -name '*.class')
+
+if [[ $total_failed != 0 ]]; then
+	echo -n "FAILED: $total_failed test"
+	[[ $total_failed > 1 ]] && echo -n "s"
+	echo ""
+fi
+
+exit $total_failed
 
