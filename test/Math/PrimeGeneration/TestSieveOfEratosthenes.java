ca07d23b7d46144608907f0fc5738f0b64d36adf
Eric <eric@eric-Aspire-4830T.(none)>
2012-08-06 20:50:11 -0400

    Created the test for the prime generator. Tests primes up to 300,000,000

--- test/Math/PrimeGeneration/TestSieveOfEratosthenes.java
+++ test/Math/PrimeGeneration/TestSieveOfEratosthenes.java
@@ -1,6 +1,7 @@
 package test.Math.PrimeGeneration;
 import java.util.*;
 import main.Math.PrimeGeneration.SieveOfEratosthenes;
+import main.Math.PrimeFactorization.TrialDivision;
 
 public class TestSieveOfEratosthenes
 {
@@ -21,7 +22,20 @@ public class TestSieveOfEratosthenes
 
 	public static void testOne(boolean printDebug)
 	{
-		SieveOfEratosthenes pf = new SieveOfEratosthenes();
-		success = false;
+		SieveOfEratosthenes soe = new SieveOfEratosthenes();
+		
+		ArrayList<Integer> primes = soe.generate(300000000);
+
+		TrialDivision td = new TrialDivision();
+		ArrayList<Long> factors;
+
+		for(int i = 0; i < 5; ++i) {
+			factors = td.doPrimeFactorization(primes.get(i));
+
+			if (factors.size() > 1) {
+				success = false;
+				break;
+			}
+		}
 	}
 }
