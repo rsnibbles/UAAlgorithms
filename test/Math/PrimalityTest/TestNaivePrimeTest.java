ebca423e520b0e49f30b1c7d126d334ce66d16f9
Eric <eric@eric-Aspire-4830T.(none)>
2012-08-06 22:31:07 -0400

    Fixed the test

--- test/Math/PrimalityTest/TestNaivePrimeTest.java
+++ test/Math/PrimalityTest/TestNaivePrimeTest.java
@@ -23,18 +23,18 @@ public class TestNaivePrimeTest
 	{
 		NaivePrimeTest npt = new NaivePrimeTest();
 
-		int[] primes = {5, 13, 19, 2003, 2011, 200003, 500009, 590000003, 79000000063};
-		int[] notPrimes = {6, 14, 21, 2005, 2013, 200005, 5000011, 590000005,790000000067 , 9223372036854775807};
+		long[] primes = {5, 13, 19, 2003, 2011, 200003, 500009,5000011, 590000003L, 79000000063L};
+		long[] notPrimes = {6, 14, 21, 2005, 2013, 200005, 5000013, 590000005L,790000000067L , 9223372036854775807L};
 
 		for (int i = 0; i < primes.length; ++i) {
-			if (!npt.primalityTest(primes[i]) {
+			if (!npt.primalityTest(primes[i])) {
 				success = false;
 				return;
 			}
 		}
 
 		for (int i = 0; i < notPrimes.length; ++i) {
-			if (npt.primalityTest(notPrimes[i]) {
+			if (npt.primalityTest(notPrimes[i])) {
 				success = false;
 				return;
 			}
