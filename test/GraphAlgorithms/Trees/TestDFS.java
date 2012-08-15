684bfd38e26abec6e46c3e9aaf5470aa7d38f454
apajx <arm77phoenix@gmail.com>
2012-08-15 20:40:53 -0300

    Temporary Commit (Moving Computers)
    Will check if the thing compiles in a bit.

--- test/GraphAlgorithms/Trees/TestDFS.java
+++ test/GraphAlgorithms/Trees/TestDFS.java
@@ -21,6 +21,99 @@ public class TestDFS
 
 	public static void testOne(boolean printDebug)
 	{
-		success = false;
+		DFS.Node[] tree = new DFS.Node[7];
+
+		for (int i = 0; i < 7; ++i) {
+			tree[i] = new DFS.Node();
+			char name = (char)('A' + i);
+			tree[i].name = Character.toString(name);
+		}
+
+		ArrayList<DFS.Node> graph = new ArrayList<DFS.Node>();
+		tree[0].children.add(tree[1]);
+		tree[0].children.add(tree[2]);
+
+		tree[1].children.add(tree[0]);
+		tree[1].children.add(tree[3]);
+		tree[1].children.add(tree[4]);
+
+		tree[2].children.add(tree[0]);
+		tree[2].children.add(tree[5]);
+		tree[2].children.add(tree[6]);
+
+		tree[3].children.add(tree[1]);
+
+		tree[4].children.add(tree[1]);
+
+		tree[5].children.add(tree[2]);
+
+		tree[6].children.add(tree[2]);
+
+		for(DFS.Node node : tree)
+			graph.add(node);
+
+		DFS dfs = new DFS();
+		ArrayList<DFS.Node> traversal = dfs.DFS(graph);
+
+		for (int i = 0; i < traversal.size(); ++i) {
+			if(!traversal.get(i).name.equals(Character.toString((char)('A' + i)))) {
+				success = false;
+				return;
+			}
+		}
+
+		traversal = dfs.DFS(graph, 6);
+		if (traversal.size() > 1 || !traversal.get(0).name.equals("G")) {
+			success = false;
+			return;
+		}
+	}
+
+	public static void testTwo(boolean printDebug)
+	{
+		DFS.Node[] tree = new DFS.Node[7];
+
+		for (int i = 0; i < 7; ++i) {
+		tree[i] = new DFS.Node();
+		char name = (char)('A' + i);
+		tree[i].name = Character.toString(name);
+		}
+
+		ArrayList<DFS.Node> graph = new ArrayList<DFS.Node>();
+		tree[0].children.add(tree[1]);
+		tree[0].children.add(tree[2]);
+
+		tree[1].children.add(tree[0]);
+		tree[1].children.add(tree[3]);
+		tree[1].children.add(tree[4]);
+
+		tree[2].children.add(tree[0]);
+		tree[2].children.add(tree[5]);
+		tree[2].children.add(tree[6]);
+
+		tree[3].children.add(tree[1]);
+
+		tree[4].children.add(tree[1]);
+
+		tree[5].children.add(tree[2]);
+
+		tree[6].children.add(tree[2]);
+
+		for(DFS.Node node : tree)
+			graph.add(node);
+
+		DFS dfs = new DFS();
+		ArrayList<DFS.Node> traversal = dfs.DFS(graph, 6);
+
+		String[] answer = { "G", "C", "A", "F", "B", "D", "E" };
+
+		for(int i = 0; i < answer.length; ++i) {
+			if (!answer[i].equals(traversal.get(i).name)) {
+				success = false;
+				return;
+			}
+		}
+
+		success = true;
 	}
 }
