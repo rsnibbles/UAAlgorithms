a5a4d6bed862b84ab4233940ffe200eb149947ba
Eric <eric@eric-Aspire-4830T.(none)>
2012-09-19 19:54:06 -0400

    Graham scan compiles and has some very basic tests that it runs against (that it fails).

--- main/GraphAlgorithms/ConvexHull/GrahamScan.java
+++ main/GraphAlgorithms/ConvexHull/GrahamScan.java
@@ -1,64 +1,76 @@
 package main.GraphAlgorithms.ConvexHull;
 
 import java.util.*;
-import java.awt.geom.*;
 
 public class GrahamScan {
 
-	public double ccw(point a,point b, point c)
+	public double ccw(Point a,Point b, Point c)
 	{
 		return (b.x-a.x)*(c.y-a.y)-(b.y-a.y)*(c.x-a.x);
 	}
 	
-	public void doGraham(ArrayList <point> points)
+	public ArrayList<Point> doGraham(ArrayList <Point> points)
 	{
-		int num=points.size();
-		ArrayList <point> increasePoints=new ArrayList <point> ();
-		point lowest=points.get(0);
-		increasePoints.add(new point());
-		for(point p : points)
+		int num = points.size();
+		ArrayList <Point> increasePoints = new ArrayList<Point>();
+		Point lowest = points.get(0);
+		increasePoints.add(new Point());
+		for(Point p : points)
 			increasePoints.add(p);
-		int j=0;
-		for(int i=0;i<points.size();i++)
+		int j = 0;
+		for(int i = 0; i < points.size(); i++)
 		{
-			if(increasePoints.get(i).y<lowest.y){
-				j=i;
-				lowest=increasePoints.get(i);
+			if(increasePoints.get(i).y < lowest.y){
+				j = i;
+				lowest = increasePoints.get(i);
 			}
 		}
 		
 		Collections.swap(increasePoints,1,j);
-		for(int temp=2;temp<increasePoints.size();temp++)
+		for(int temp = 2; temp < increasePoints.size(); temp++)
 		{
-			increasePoints.get(temp).radian=Math.atan((increasePoints.get(1).y-increasePoints.get(temp).y)/(increasePoints.get(1).x-increasePoints.get(temp).x));
+			increasePoints.get(temp).radian = Math.atan((increasePoints.get(1).y - increasePoints.get(temp).y)
+					/ (increasePoints.get(1).x - increasePoints.get(temp).x));
 			
 		}
 		Collections.sort(increasePoints);
 		
-		increasePoints.set(0,points.get(num));
+		increasePoints.set(0,points.get(num - 1));
 		
-		int m=1;
-		for (int i=2;i<=num;i++)
+		int m = 1;
+		for (int i = 2; i <= num; i++)
 		{
-			while (ccw(increasePoints.get(m-1),increasePoints.get(m),increasePoints.get(i))<=0)
+			while (ccw(increasePoints.get(m - 1), increasePoints.get(m), increasePoints.get(i)) <= 0)
 			{
-				if(m>1)
-					m-=1;
-				else if(i==num)
+				if(m > 1)
+					m -= 1;
+				else if(i == num)
 					break;
 				else
-					i+=1;
+					i += 1;
 			}
 			m++;
-			Collections.swap(increasePoints,m,i);
+			Collections.swap(increasePoints, m, i);
 		}
+		
+		return increasePoints;
 	}
-	public class point implements Comparable<point>{
+	
+	public class Point implements Comparable<Point>{
 		double x;
 		double y;
 		double radian;
 		
-		public int compareTo(point other)
+		public Point() {
+			
+		}
+		
+		public Point(double x, double y) {
+			this.x = x;
+			this.y = y;
+		}
+		
+		public int compareTo(Point other)
 		{
 			return (int) (this.radian-other.radian);
 		}
--- test/GraphAlgorithms/ConvexHull/TestGrahamScan.java
+++ test/GraphAlgorithms/ConvexHull/TestGrahamScan.java
@@ -6,12 +6,114 @@ public class TestGrahamScan {
 	public static boolean success = false;
 	
 	public static void main(String[] args) {
+		success = true;
+		
+		testOne(true);
+		testTwo(true);
+		testThree(true);
+		
 		if (!success) {
 			System.out.println("FAILED: TestGrahamScan");
 			System.exit(1);
 		}
 		else {
 			System.exit(0);
+		}
+	}
+	
+	public static void testOne(boolean printDebug) {
+		GrahamScan gs = new GrahamScan();
+		ArrayList<GrahamScan.Point> points = new ArrayList<GrahamScan.Point>();
+		
+		
+		points.add(gs.new Point(4, 3));
+		points.add(gs.new Point(-5, 0));
+		points.add(gs.new Point(-5, 2));
+		points.add(gs.new Point(-3, 1));
+		points.add(gs.new Point(-1, 4));
+		points.add(gs.new Point(-1, -4));
+		points.add(gs.new Point(0, -3));
+		points.add(gs.new Point(1, 3));
+		points.add(gs.new Point(2, 1));
+		points.add(gs.new Point(3, 4));
+		
+		points.add(gs.new Point(5, -2));
+		points.add(gs.new Point(5, 1));
+		
+		Collections.shuffle(points);
+		
+		ArrayList<GrahamScan.Point> answer = gs.doGraham(points);
+		
+		if (answer.size() != 8) {
+			success = false;
+			return;
+		}
+	}
+	
+	public static void testTwo(boolean printDebug) {
+		ArrayList<GrahamScan.Point> points = new ArrayList<GrahamScan.Point>();
+		GrahamScan gs = new GrahamScan();
+		
+		points.add(gs.new Point(0, 0));
+		points.add(gs.new Point(1, 1));
+		points.add(gs.new Point(5, 1));
+		points.add(gs.new Point(3, 2));
+		points.add(gs.new Point(2, 3));
+		points.add(gs.new Point(4, 3));
+		points.add(gs.new Point(1, 4));
+		points.add(gs.new Point(6, 4));
+		points.add(gs.new Point(3, 5));
+		points.add(gs.new Point(8, 5));
+		points.add(gs.new Point(4, 7));
+		
+		Collections.shuffle(points);
+		
+		ArrayList<GrahamScan.Point> answer = gs.doGraham(points);
+		
+		if (answer.size() != 5) {
+			success = false;
+			return;
+		}
+	}
+	
+	public static void testThree(boolean printDebug) {
+		ArrayList<GrahamScan.Point> points = new ArrayList<GrahamScan.Point>();
+		GrahamScan gs = new GrahamScan();
+		
+		points.add(gs.new Point(0, 0));
+		points.add(gs.new Point(1, 1));
+		points.add(gs.new Point(2, 2));
+		points.add(gs.new Point(-1, -1));
+		points.add(gs.new Point(-2, -2));
+		points.add(gs.new Point(1, -1));
+		points.add(gs.new Point(-1, 1));
+		points.add(gs.new Point(-2, 2));
+		points.add(gs.new Point(2, -2));
+		
+		//right
+		points.add(gs.new Point(3, -3));
+		points.add(gs.new Point(3, -2));
+		points.add(gs.new Point(3, 3));
+		//top
+		points.add(gs.new Point(3, 3));
+		points.add(gs.new Point(1, 3));
+		points.add(gs.new Point(-3, 3));
+		//bot
+		points.add(gs.new Point(3, -3));
+		points.add(gs.new Point(0, -3));
+		points.add(gs.new Point(-3, -3));
+		//left
+		points.add(gs.new Point(-3, -3));
+		points.add(gs.new Point(-3, -2));
+		points.add(gs.new Point(-3, 3));
+		
+		Collections.shuffle(points);
+		
+		ArrayList<GrahamScan.Point> answer = gs.doGraham(points);
+		
+		if (answer.size() != 8) {
+			success = false;
+			return;
 		}
 	}
 }
