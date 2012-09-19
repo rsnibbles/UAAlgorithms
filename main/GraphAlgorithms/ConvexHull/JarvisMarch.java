819ebee1f44429bbdc7259fdb82e9e98e9243d27
Eric <eric@eric-Aspire-4830T.(none)>
2012-09-19 11:22:12 -0400

    Updated the jarvis march code and tests. Appears to work but only adds extreme points and not intermediate lines on the same point

--- main/GraphAlgorithms/ConvexHull/JarvisMarch.java
+++ main/GraphAlgorithms/ConvexHull/JarvisMarch.java
@@ -5,26 +5,63 @@ import java.awt.geom.*;
 
 public class JarvisMarch {
 
-	public ArrayList doJarvis(ArrayList <Point2D> points) {
-		Point2D leftMost=points.get(0);
-		Point2D endpoint;
-		ArrayList <Point2D> pointsAdded=new ArrayList <Point2D> ();
-		int i=0;
-		for(Point2D p: points) {
-			if(p.getX()<leftMost.getX())
-				leftMost=p;
+	public ArrayList<Point2D.Double> doJarvis(ArrayList <Point2D.Double> points) {
+		Point2D.Double leftMost = points.get(0);
+		Point2D.Double next;
+		ArrayList<Point2D.Double> pointsAdded = new ArrayList<Point2D.Double> ();
+		
+		for(Point2D.Double p : points) {
+			if(p.x < leftMost.x || (p.x == leftMost.x && p.y < leftMost.y))
+				leftMost = p;
 		}
-		do {
-			pointsAdded.add(leftMost);
-			endpoint=points.get(0);
-			for(int j=1;j<points.size();j++) {
-				if((endpoint==leftMost)||(points.get(j).getX()<pointsAdded.get(i).getX()))
-					endpoint=points.get(j);
+		
+		pointsAdded.add(leftMost);
+		
+		int i = 0;
+		while (i < pointsAdded.size()) {
+			if (i > 0) {
+				next = nextHullPoint(points,pointsAdded.get(i - 1), pointsAdded.get(i));
 			}
-			i++;
-			leftMost=endpoint;
-		}while(endpoint != pointsAdded.get(0));
+			else {
+				next = nextHullPoint(points, pointsAdded.get(i), pointsAdded.get(i));
+			}
+			
+			
+			if (next != pointsAdded.get(0)) {
+				pointsAdded.add(next);
+			}
+			
+			++i;
+		}
 		
 		return pointsAdded;
 	}
+	
+	private double distance(Point2D.Double p1, Point2D.Double p2) {
+		double dx = p2.x - p1.x;
+		double dy = p2.y - p1.y;
+		
+		return dx * dx + dy * dy;
+	}
+	
+	private int turn(Point2D.Double p, Point2D.Double q, Point2D.Double r) {
+		return Double.compare((q.x - p.x) * (r.y - p.y) - (r.x - p.x) * (q.y - p.y), 0);
+	}
+	
+	private Point2D.Double nextHullPoint(ArrayList<Point2D.Double> points,Point2D.Double prev, Point2D.Double p) {
+		Point2D.Double q = prev;
+		
+		for (Point2D.Double r : points) {
+			int turn = turn(p, q, r);
+			double d1 = distance(p, r);
+			double d2 = distance(p, q);
+			if (turn == 1 || (turn == 0 && d1 > d2)) {
+				q = r;
+			}
+		}
+		
+		return q;
+	}
+	
+	
 }
--- test/GraphAlgorithms/ConvexHull/TestJarvisMarch.java
+++ test/GraphAlgorithms/ConvexHull/TestJarvisMarch.java
@@ -1,17 +1,119 @@
 package test.GraphAlgorithms.ConvexHull;
+import java.awt.geom.Point2D;
 import java.util.*;
 import main.GraphAlgorithms.ConvexHull.JarvisMarch;
 
 public class TestJarvisMarch {
-	public static boolean success = false;
+	public static boolean success;
 	
 	public static void main(String[] args) {
+		success = true;
+		
+		testOne(true);
+		testTwo(true);
+		testThree(true);
+		
 		if (!success) {
 			System.out.println("FAILED: TestJarvisMarch");
 			System.exit(1);
 		}
 		else {
 			System.exit(0);
+		}
+	}
+	
+	public static void testOne(boolean printDebug) {
+		ArrayList<Point2D.Double> points = new ArrayList<Point2D.Double>();
+		
+		points.add(new Point2D.Double(4, 3));
+		points.add(new Point2D.Double(-5, 0));
+		points.add(new Point2D.Double(-5, 2));
+		points.add(new Point2D.Double(-3, 1));
+		points.add(new Point2D.Double(-1, 4));
+		points.add(new Point2D.Double(-1, -4));
+		points.add(new Point2D.Double(0, -3));
+		points.add(new Point2D.Double(1, 3));
+		points.add(new Point2D.Double(2, 1));
+		points.add(new Point2D.Double(3, 4));
+		
+		points.add(new Point2D.Double(5, -2));
+		points.add(new Point2D.Double(5, 1));
+		
+		Collections.shuffle(points);
+		
+		JarvisMarch jm = new JarvisMarch();
+		ArrayList<Point2D.Double> answer = jm.doJarvis(points);
+		
+		if (answer.size() != 8) {
+			success = false;
+			return;
+		}
+	}
+	
+	public static void testTwo(boolean printDebug) {
+		ArrayList<Point2D.Double> points = new ArrayList<Point2D.Double>();
+		
+		points.add(new Point2D.Double(0, 0));
+		points.add(new Point2D.Double(1, 1));
+		points.add(new Point2D.Double(5, 1));
+		points.add(new Point2D.Double(3, 2));
+		points.add(new Point2D.Double(2, 3));
+		points.add(new Point2D.Double(4, 3));
+		points.add(new Point2D.Double(1, 4));
+		points.add(new Point2D.Double(6, 4));
+		points.add(new Point2D.Double(3, 5));
+		points.add(new Point2D.Double(8, 5));
+		points.add(new Point2D.Double(4, 7));
+		
+		Collections.shuffle(points);
+		
+		JarvisMarch jm = new JarvisMarch();
+		ArrayList<Point2D.Double> answer = jm.doJarvis(points);
+		
+		if (answer.size() != 5) {
+			success = false;
+			return;
+		}
+	}
+	
+	public static void testThree(boolean printDebug) {
+		ArrayList<Point2D.Double> points = new ArrayList<Point2D.Double>();
+		
+		points.add(new Point2D.Double(0, 0));
+		points.add(new Point2D.Double(1, 1));
+		points.add(new Point2D.Double(2, 2));
+		points.add(new Point2D.Double(-1, -1));
+		points.add(new Point2D.Double(-2, -2));
+		points.add(new Point2D.Double(1, -1));
+		points.add(new Point2D.Double(-1, 1));
+		points.add(new Point2D.Double(-2, 2));
+		points.add(new Point2D.Double(2, -2));
+		
+		//right
+		points.add(new Point2D.Double(3, -3));
+		points.add(new Point2D.Double(3, -2));
+		points.add(new Point2D.Double(3, 3));
+		//top
+		points.add(new Point2D.Double(3, 3));
+		points.add(new Point2D.Double(1, 3));
+		points.add(new Point2D.Double(-3, 3));
+		//bot
+		points.add(new Point2D.Double(3, -3));
+		points.add(new Point2D.Double(0, -3));
+		points.add(new Point2D.Double(-3, -3));
+		//left
+		points.add(new Point2D.Double(-3, -3));
+		points.add(new Point2D.Double(-3, -2));
+		points.add(new Point2D.Double(-3, 3));
+		
+		Collections.shuffle(points);
+		
+		JarvisMarch jm = new JarvisMarch();
+		ArrayList<Point2D.Double> answer = jm.doJarvis(points);
+		
+		if (answer.size() != 8) {
+			success = false;
+			return;
 		}
 	}
 }
