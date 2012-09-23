5c871b8425332288241812ea0605cacb48ef7839
Eric Arndt <arndt.eric@gmail.com>
2012-09-23 11:58:45 -0400

    Updated floyds for path reconstruction
    Completely changed how the path for floyds is set up. No longer returns
    the ArrayList<Integer[][]> but instead returns Node[][]. Changed how the
    path is reconstructed so the path of any Node[][] can be reconstructed.
    Also added some tests for path reconstruction.

--- main/GraphAlgorithms/PathFinding/Floyds.java
+++ main/GraphAlgorithms/PathFinding/Floyds.java
@@ -23,70 +23,68 @@ public class Floyds {
 				}
 			}
 		}
+		
 		return aMatrix;
 	}
+	
 	/*
 	 Integer is used instead of int because we wish to allow the possibility of an entry
 	 being "null", which is otherwise impossible. More specifically, we want entries in
 	 nextMatrix to be null when the relationship to adjacencyMatrix turns up an "Infinity".
 	 Likewise, we need to allow Integer to equal infinity (or just the max).
 		
-	 This implementation of Floyds will allow us to re-establish the path instesad of being
+	 This implementation of Floyds will allow us to re-establish the path instead of being
 	 limited to only the shortest distance, the time complexity is the same but the code
 	 complexity is a little more involved.
 	 */
-	Integer[][] globalAdjacencyMatrix;
-	Integer[][] globalNextMatrix;
-
-	public ArrayList<Integer[][]> floyds(Integer[][] adjacencyMatrix, Integer[][] nextMatrix) {
+	public Node[][] floyds(Node[][] aMatrix) {
 		// Used as infinity
 		final int I = Integer.MAX_VALUE;
 		
-		ArrayList<Integer[][]> Matrices = new ArrayList<Integer[][]>();
-		Integer[][] aMatrix = adjacencyMatrix;
 		int n = aMatrix.length;
-		if (nextMatrix == null) //passing next is optional
-		{
-			nextMatrix = new Integer[n][n];
-		}
 
 		for (int k = 0; k < n; ++k) {
 			for (int i = 0; i < n; ++i) {
 				for (int j = 0; j < n; ++j) {
-					if (aMatrix[i][k] != I && aMatrix[k][j] != I
-							&& aMatrix[i][k] + aMatrix[k][j] < aMatrix[i][j]) {
-						aMatrix[i][j] = new Integer(aMatrix[i][k] + aMatrix[k][j]);
-						nextMatrix[i][j] = new Integer(k);
+					if (aMatrix[i][k].d != I && aMatrix[k][j].d != I
+							&& aMatrix[i][k].d + aMatrix[k][j].d < aMatrix[i][j].d) {
+						aMatrix[i][j].d = aMatrix[i][k].d + aMatrix[k][j].d;
+						aMatrix[i][j].k = k;
 					}
 				}
 			}
 		}
 
-		//This is done to allow getPath to talk with these matrices (for testing purposes)
-		globalAdjacencyMatrix = aMatrix;
-		globalNextMatrix = nextMatrix;
-		Matrices.add(aMatrix);
-		Matrices.add(nextMatrix);
-
-		return Matrices;
+		return aMatrix;
 	}
 
 	/* 
 	 getPath assumes it has adjacencyMatrix and nextMatrix in scope, as well as an "infinity"
 	 */
-	public String getPath(int i, int j) {
+	public String getPath(Node[][] matrix, int i, int j) {
+		return i + getPathHelper(matrix, i, j) + j + "\n";
+	}
+	
+	public String getPathHelper(Node[][] matrix, int i, int j) {
 		// Used as infinity
 		final int I = Integer.MAX_VALUE;
 		
-		if (globalAdjacencyMatrix[i][j] >= I) // >= just in case
+		if (matrix[i][j].d >= I) // >= just in case
 		{
 			return "no path";
 		}
-		Integer intermediate = globalNextMatrix[i][j];
+		
+		Integer intermediate = matrix[i][j].k;
+		
 		if (intermediate == null) {
 			return " "; //There is an edge from i to j, with no vertices between
 		} else {
-			return getPath(i, intermediate) + intermediate.toString() + getPath(intermediate, j);
+			return getPathHelper(matrix, i, intermediate) + intermediate + getPathHelper(matrix, intermediate, j);
 		}
+	}
+	
+	public class Node {
+		public Integer d;
+		public Integer k;
 	}
 }
--- test/GraphAlgorithms/PathFinding/TestFloyds.java
+++ test/GraphAlgorithms/PathFinding/TestFloyds.java
@@ -9,6 +9,8 @@ public class TestFloyds {
 
 	public static void main(String[] args) {
 		testOne(false);
+		testTwo(false);
+		
 		if (!success) {
 			System.out.println("FAILED: TestFloyds");
 			System.exit(1);
@@ -43,9 +45,20 @@ public class TestFloyds {
 	 5 | 8 | 7 | 5 | 10| 0 
 	 */
 	public static void testOne(boolean printDebug) {
-		int I = Integer.MAX_VALUE;
-		int[][] testMatrix = {{0, 1, I, 5, 100}, {1, 0, 100, 3, 7}, {I, 100, 0, 100, 5}, {5, 3, 100, 0, 100}, {100, 7, 5, 100, 0}};
-		int[][] solMatrix = {{0, 1, 13, 4, 8}, {1, 0, 12, 3, 7}, {13, 12, 0, 15, 5}, {4, 3, 15, 0, 10}, {8, 7, 5, 10, 0}};
+		final int I = Integer.MAX_VALUE;
+		int[][] testMatrix = {
+			{0, 1, I, 5, 100}, 
+			{1, 0, 100, 3, 7}, 
+			{I, 100, 0, 100, 5}, 
+			{5, 3, 100, 0, 100}, 
+			{100, 7, 5, 100, 0}};
+		int[][] solMatrix = {
+			{0, 1, 13, 4, 8}, 
+			{1, 0, 12, 3, 7}, 
+			{13, 12, 0, 15, 5}, 
+			{4, 3, 15, 0, 10}, 
+			{8, 7, 5, 10, 0}};
+		
 		int n = 5;
 		int[][] result = new int[5][5];
 
@@ -64,6 +77,56 @@ public class TestFloyds {
 			printArray(result);
 			System.out.println();
 			printArray(solMatrix);
+		}
+	}
+	
+	public static void testTwo(boolean printDebug) {
+		final int I = Integer.MAX_VALUE;
+		
+		Floyds fyd = new Floyds();
+		Integer[][] test = {
+			{0, 1, I, 5, 100}, 
+			{1, 0, 100, 3, 7}, 
+			{I, 100, 0, 100, 5}, 
+			{5, 3, 100, 0, 100}, 
+			{100, 7, 5, 100, 0}};
+		int[][] solMatrix = {
+			{0, 1, 13, 4, 8}, 
+			{1, 0, 12, 3, 7}, 
+			{13, 12, 0, 15, 5}, 
+			{4, 3, 15, 0, 10}, 
+			{8, 7, 5, 10, 0}};
+		
+		Floyds.Node[][] testMatrix = new Floyds.Node[5][5];
+		
+		for (int i = 0; i < testMatrix.length; ++i) {
+			for (int j = 0; j < testMatrix.length; ++j) {
+				testMatrix[i][j] = fyd.new Node();
+				testMatrix[i][j].d = test[i][j];
+			}
+		}
+		
+		
+		Floyds.Node[][] answer = fyd.floyds(testMatrix);
+		
+		for (int i = 0; i < answer.length; ++i) {
+			for (int j = 0; j < answer.length; ++j) {
+				if (answer[i][j].d != solMatrix[i][j]) {
+					success = false;
+				}
+			}
+		}
+		
+		if(!fyd.getPath(answer, 0, 3).trim().equals("0 1 3")) {
+			success = false;
+		}
+		
+		if(!fyd.getPath(answer, 1, 3).trim().equals("1 3")) {
+			success = false;
+		}
+		
+		if(!fyd.getPath(answer, 2, 0).trim().equals("2 4 1 0")) {
+			success = false;
 		}
 	}
 
