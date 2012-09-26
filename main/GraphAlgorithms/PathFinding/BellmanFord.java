9930adb7f087c4852ba9e82934c6cd73f44fbba7
Eric <eric@eric-Aspire-4830T.(none)>
2012-09-26 18:57:01 -0400

    Added tests for Bellman-Ford algorithm. Bellman-Ford now returns null when there is a graph with a negative cycle.

--- main/GraphAlgorithms/PathFinding/BellmanFord.java
+++ main/GraphAlgorithms/PathFinding/BellmanFord.java
@@ -1,52 +1,59 @@
 package main.GraphAlgorithms.PathFinding;
+
 import java.util.*;
 
 public class BellmanFord {
 
 	public ArrayList<Node> doBellford(ArrayList<Node> nodes, ArrayList<Edge> edges, Node source) {
-		for(Node n: nodes) {
-			if( n == source )
+		for (Node n : nodes) {
+			if (n == source) {
 				n.distance = 0;
-			else
+			} else {
 				n.distance = Integer.MAX_VALUE;
+			}
+
 			n.predecessor = null;
 		}
-		
-		for(int i = 1; i < nodes.size(); i++) {
-			for(Edge e: edges) {
+
+		for (int i = 1; i < nodes.size(); i++) {
+			for (Edge e : edges) {
 				Node s = e.source;
 				Node d = e.destination;
-				if(s.distance + e.weight < d.distance) {
+				if (s.distance + e.weight < d.distance) {
 					d.distance = s.distance + e.weight;
 					d.predecessor = s;
 				}
 			}
 		}
-		
-		for(Edge e: edges) {
+
+		for (Edge e : edges) {
 			Node s = e.source;
 			Node d = e.destination;
-			if(s.distance + e.weight < d.distance)
-				System.out.println("Graph contains a negative-weight cycle");
+			if (s.distance + e.weight < d.distance) {
+				return null;
+			}
 		}
-		
+
 		return nodes;
 	}
-	
+
 	public class Node {
-		Node predecessor = null;
-		int distance;
-		
+
+		public Node predecessor = null;
+		public double distance;
+
 		public Node() {
-			distance = 0;
+			distance = Double.MAX_VALUE;
 		}
 	}
 
 	public class Edge {
-		Node source, destination;
-		int weight;
-		
-		public Edge(Node s, Node d, int w) {
+
+		public Node source;
+		public Node destination;
+		public double weight;
+
+		public Edge(Node s, Node d, double w) {
 			source = s;
 			destination = d;
 			weight = w;
--- test/GraphAlgorithms/PathFinding/TestBellmanFord.java
+++ test/GraphAlgorithms/PathFinding/TestBellmanFord.java
@@ -1,19 +1,147 @@
 package test.GraphAlgorithms.PathFinding;
+
 import java.util.*;
 import main.GraphAlgorithms.PathFinding.BellmanFord;
 
 public class TestBellmanFord {
+
 	public static boolean success;
-	
+
 	public static void main(String[] args) {
-		success = false;
+		success = true;
+
+		testOne(true);
+		testTwo(true);
+		testThree(true);
+		testFour(true);
+		testFive(true);
 
 		if (!success) {
 			System.out.println("FAILED: TestBellmanFord");
 			System.exit(1);
-		}
-		else {
+		} else {
 			System.exit(0);
+		}
+	}
+
+	public static void testOne(boolean printDebug) {
+		BellmanFord bf = new BellmanFord();
+
+		ArrayList<BellmanFord.Node> nodes = new ArrayList<BellmanFord.Node>();
+		ArrayList<BellmanFord.Edge> edges = new ArrayList<BellmanFord.Edge>();
+
+		nodes.add(bf.new Node());
+		nodes.add(bf.new Node());
+
+		edges.add(bf.new Edge(nodes.get(0), nodes.get(1), 1));
+
+		ArrayList<BellmanFord.Node> answer = bf.doBellford(nodes, edges, nodes.get(0));
+
+		if (answer.get(0).distance != 0) {
+			success = false;
+		}
+
+		if (answer.get(1).distance != 1) {
+			success = false;
+		}
+	}
+
+	public static void testTwo(boolean printDebug) {
+		BellmanFord bf = new BellmanFord();
+
+		ArrayList<BellmanFord.Node> nodes = new ArrayList<BellmanFord.Node>();
+		ArrayList<BellmanFord.Edge> edges = new ArrayList<BellmanFord.Edge>();
+
+		nodes.add(bf.new Node());
+		nodes.add(bf.new Node());
+
+		edges.add(bf.new Edge(nodes.get(0), nodes.get(1), -1));
+
+		ArrayList<BellmanFord.Node> answer = bf.doBellford(nodes, edges, nodes.get(0));
+
+		if (answer.get(0).distance != 0) {
+			success = false;
+		}
+
+		if (answer.get(1).distance != -1) {
+			success = false;
+		}
+	}
+
+	public static void testThree(boolean printDebug) {
+		BellmanFord bf = new BellmanFord();
+
+		ArrayList<BellmanFord.Node> nodes = new ArrayList<BellmanFord.Node>();
+		ArrayList<BellmanFord.Edge> edges = new ArrayList<BellmanFord.Edge>();
+
+		nodes.add(bf.new Node());
+		nodes.add(bf.new Node());
+		nodes.add(bf.new Node());
+		nodes.add(bf.new Node());
+
+		edges.add(bf.new Edge(nodes.get(0), nodes.get(1), 5));
+		edges.add(bf.new Edge(nodes.get(0), nodes.get(2), 5));
+		edges.add(bf.new Edge(nodes.get(1), nodes.get(3), 5));
+		edges.add(bf.new Edge(nodes.get(2), nodes.get(3), -5));
+
+		ArrayList<BellmanFord.Node> answer = bf.doBellford(nodes, edges, nodes.get(0));
+
+		if (answer.get(0).distance != 0) {
+			success = false;
+		}
+
+		if (answer.get(1).distance != 5) {
+			success = false;
+		}
+
+		if (answer.get(2).distance != 5) {
+			success = false;
+		}
+
+		if (answer.get(3).distance != 0) {
+			success = false;
+		}
+	}
+
+	public static void testFour(boolean printDebug) {
+		BellmanFord bf = new BellmanFord();
+
+		ArrayList<BellmanFord.Node> nodes = new ArrayList<BellmanFord.Node>();
+		ArrayList<BellmanFord.Edge> edges = new ArrayList<BellmanFord.Edge>();
+
+		nodes.add(bf.new Node());
+		nodes.add(bf.new Node());
+
+		edges.add(bf.new Edge(nodes.get(0), nodes.get(1), -1));
+		edges.add(bf.new Edge(nodes.get(1), nodes.get(0), -1));
+
+
+		ArrayList<BellmanFord.Node> answer = bf.doBellford(nodes, edges, nodes.get(0));
+
+		if (answer != null) {
+			success = false;
+		}
+	}
+
+	public static void testFive(boolean printDebug) {
+		BellmanFord bf = new BellmanFord();
+
+		ArrayList<BellmanFord.Node> nodes = new ArrayList<BellmanFord.Node>();
+		ArrayList<BellmanFord.Edge> edges = new ArrayList<BellmanFord.Edge>();
+
+		nodes.add(bf.new Node());
+		nodes.add(bf.new Node());
+		nodes.add(bf.new Node());
+
+		edges.add(bf.new Edge(nodes.get(0), nodes.get(1), 2));
+		edges.add(bf.new Edge(nodes.get(1), nodes.get(2), 2));
+		edges.add(bf.new Edge(nodes.get(2), nodes.get(0), -5));
+
+
+		ArrayList<BellmanFord.Node> answer = bf.doBellford(nodes, edges, nodes.get(0));
+
+		if (answer != null) {
+			success = false;
 		}
 	}
 }
