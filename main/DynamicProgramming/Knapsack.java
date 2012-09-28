04a5d0e849756e6df235bd6ded3ef56761fd52bf
Eric <eric@eric-Aspire-4830T.(none)>
2012-09-28 19:05:22 -0400

    We have Knapsack with working tests!

---
+++ main/DynamicProgramming/Knapsack.java
@@ -0,0 +1,58 @@
+package main.DynamicProgramming;
+
+import java.util.*;
+
+/*
+ * O(nm) where n is number of items and m is the total capacity
+ */
+public class Knapsack {
+	public long knapsack(ArrayList<Item> items, int cap) {
+		long[][] backpack = new long[items.size()][cap + 1];
+		
+		for (int i = 0; i < items.size(); ++i) {
+			Item c = items.get(i);
+			
+			for (int j = 1; j < cap + 1; ++j) {
+				// first item
+				if (i == 0) {
+					// item fits
+					if (j - c.weight >= 0) {
+						backpack[i][j] = c.value;
+					// item does not fit
+					} else {
+						backpack[i][j] = 0;
+					}		
+				} else {
+					// item fits
+					if (j - c.weight >= 0) {
+						long back = backpack[i][j - 1];
+						long up = backpack[i - 1][j];
+						long diag = c.value + backpack[i - 1][j - c.weight];
+						
+						backpack[i][j] = Math.max(back, Math.max(up, diag));
+					}
+					else {
+						//if (j > 0) {
+							//backpack[i][j] = Math.max(backpack[i - 1][j], backpack[i][j - 1]);
+						//} else {
+							backpack[i][j] = backpack[i - 1][j];
+						//}
+						
+					}
+				}
+			}
+		}
+		
+		return backpack[items.size() - 1][cap];
+	}
+	
+	public class Item {
+		public long value;
+		public int weight;
+		
+		public Item( int w, long v) {
+			value = v;
+			weight = w;
+		}
+	}
+}
